45d6694441048b445c7d9d56e9de079e30cfe29d
Ricardo Fideles <ricardo@ricardofideles.com.br>
2018-02-25 17:41:56 -0300

    ajuste

--- components/UdaciSlider.js
+++ components/UdaciSlider.js
@@ -1,10 +1,20 @@
-import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import React, { Component } from 'react';
+import { View, Text, Slider } from 'react-native';
 
-export default function UdaciSlider () {
-    return (
-        <View>
-            <Text>UdaciSlider</Text>
-        </View>
-    )
+export default function UdaciSlider({ max, unit, step, value, onChange }) {
+  return (
+    <View>
+      <Slider
+        step={step}
+        value={value}
+        maximumValue={max}
+        maximumValue={0}
+        onValueChange={onChange}
+      />
+      <View>
+        <Text>{value}</Text>
+        <Text>{unit}</Text>
+      </View>
+    </View>
+  );
 }
